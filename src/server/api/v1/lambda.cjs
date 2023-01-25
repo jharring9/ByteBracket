e3797f93bdb2bfd76525964ea72fcccf9d0795df
Lucas Goldman <lucasgoldman@optonline.net>
2023-01-25 14:09:33 -0600

    Connect Lambda

--- src/server/api/v1/lambda.cjs
+++ src/server/api/v1/lambda.cjs
@@ -3,25 +3,43 @@ const AWS = require("aws-sdk");
 module.exports = (app) => {
   app.post("/v1/lambda", async (req, res) => {
     // TODO -- validate input before passing to lambda
-    // AWS.config.update({ region: "us-east-1" });
-    // const invokeLambda = async () => {
-    //   const params = {
-    //     FunctionName: "ByteBracket",
-    //     Payload: JSON.stringify(req.body),
-    //   };
-    //   const result = await new AWS.Lambda().invoke(params).promise();
-    //   return res.status(200).send(JSON.parse(result.Payload));
-    // };
-    // invokeLambda().catch((error) => {
-    //   console.error("Error invoking Lambda", error);
-    //   return res.status(400).send("Error invoking ByteBracket Lambda.");
-    // });
-
-    const response = {
-      bracket: generateBracket(),
-      top25: generateTop25(),
+    AWS.config.update({ region: "us-east-1" });
+    const invokeLambda = async () => {
+      const params = {
+        FunctionName: "ByteBracket",
+        Payload: JSON.stringify(req.body),
+      };
+      const result = await new AWS.Lambda().invoke(params).promise();
+      let data = JSON.parse(result.Payload);
+      console.log(data);
+      let top25Schools = data['Schools'].slice(0,25)
+      data['top25'] = top25Schools.map((school, index) => {
+        let r = Math.floor(Math.random()*25)
+            return ({
+              team: school,
+              rank: index + 1,
+              apRank: r,
+              diff: r - (index + 1),
+            })
+          }
+      );
+      return res.status(200).send(data);
     };
-    res.send(response);
+    invokeLambda().catch((error) => {
+      console.error("Error invoking Lambda", error);
+      return res.status(400).send("Error invoking ByteBracket Lambda.");
+    });
+
+    // const response = {
+    //   bracket: generateBracket(),
+    //   top25: generateTop25(),
+    // };
+
+    // const response = {
+    //   bracket: generateBracket(),
+    //   schools: generateTop25(),
+    // };
+    // res.send(response);
   });
 };
 
