dde675e4e3a1019c0b4cee014d5a9cd8f5cb7e4c
Jack Harrington <47326372+jharring9@users.noreply.github.com>
2023-01-26 14:30:39 -0600

    Updated profile page

--- src/client/components/account-flow/Profile.js
+++ src/client/components/account-flow/Profile.js
@@ -16,37 +16,40 @@ export const Profile = ({ user }) => {
             Profile
           </h2>
           <p className="mt-1 text-sm text-gray-500">
-            By default, this information can be viewed publicly.
+            By default, this information cannot be viewed publicly.
           </p>
         </div>
-
-        <div className="mt-6 flex flex-col lg:flex-row">
-          <div className="flex-grow space-y-6">
-            <div>
-              <label
-                htmlFor="username"
-                className="block text-sm font-medium text-gray-700"
-              >
-                Username
-              </label>
-              <div className="mt-1 flex rounded-md shadow-sm">
-                <span className="inline-flex items-center rounded-l-md border border-r-0 border-gray-300 bg-gray-50 px-3 text-gray-500 sm:text-sm">
-                  bytebracket.io/
-                </span>
-                <input
-                  type="text"
-                  name="username"
-                  id="username"
-                  autoComplete="username"
-                  className="block w-full min-w-0 flex-grow rounded-none rounded-r-md border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
-                  defaultValue={user.username}
-                />
-              </div>
-            </div>
-          </div>
-        </div>
-
         <div className="mt-6 grid grid-cols-12 gap-6">
+          <div className="col-span-12 sm:col-span-6">
+            <label
+              htmlFor="username"
+              className="block text-sm font-medium text-gray-700"
+            >
+              Username
+            </label>
+            <input
+              type="text"
+              name="username"
+              id="username"
+              className="mt-1 block w-full rounded-md border border-gray-300 py-2 px-3 shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+              defaultValue={user.username}
+            />
+          </div>
+          <div className="col-span-12 sm:col-span-6">
+            <label
+              htmlFor="email"
+              className="block text-sm font-medium text-gray-700"
+            >
+              Email
+            </label>
+            <input
+              type="email"
+              name="email"
+              id="email"
+              className="mt-1 block w-full rounded-md border border-gray-300 py-2 px-3 shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+              defaultValue={user.first}
+            />
+          </div>
           <div className="col-span-12 sm:col-span-6">
             <label
               htmlFor="first-name"
@@ -58,12 +61,10 @@ export const Profile = ({ user }) => {
               type="text"
               name="first-name"
               id="first-name"
-              autoComplete="given-name"
               className="mt-1 block w-full rounded-md border border-gray-300 py-2 px-3 shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
               defaultValue={user.first}
             />
           </div>
-
           <div className="col-span-12 sm:col-span-6">
             <label
               htmlFor="last-name"
@@ -75,7 +76,6 @@ export const Profile = ({ user }) => {
               type="text"
               name="last-name"
               id="last-name"
-              autoComplete="family-name"
               className="mt-1 block w-full rounded-md border border-gray-300 py-2 px-3 shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
               defaultValue={user.last}
             />
