c52664fed46e1d983665d9543b800e9e58f0a722
Lucas Goldman <58446351+lucasrgoldman@users.noreply.github.com>
2023-01-26 16:44:47 -0600

    Use real AP Rankings (#24)

--- src/server/api/v1/lambda.cjs
+++ src/server/api/v1/lambda.cjs
@@ -1,5 +1,33 @@
 const AWS = require("aws-sdk");
 
+const AP_RANKINGS = {
+  Purdue: 1,
+  Alabama: 2,
+  Houston: 3,
+  Tennessee: 4,
+  "Kansas State" : 5,
+  Arizona: 6,
+  Virginia: 7,
+  UCLA: 8,
+  Kansas: 9,
+  Texas: 10,
+  TCU: 11,
+  "Iowa State": 12,
+  Xavier: 13,
+  Gonzaga: 14,
+  Auburn: 15,
+  Marquette: 16,
+  Baylor: 17,
+  Charleston: 18,
+  Connecticut: 19,
+  Miami: 20,
+  "Florida Atlantic": 21,
+  "Saint Mary": 22,
+  Providence: 23,
+  Clemson: 24,
+  "New Mexico": 25
+}
+
 module.exports = (app) => {
   app.post("/v1/lambda", async (req, res) => {
     // TODO -- validate input before passing to lambda
@@ -14,11 +42,11 @@ module.exports = (app) => {
       let top25Schools = data["Schools"].slice(0, 25);
       data["bracket"] = generateBracket(data["Schools"].slice(0, 64));
       data["top25"] = top25Schools.map((school, index) => {
-        let r = Math.floor(Math.random() * 25);
+        let r = school in AP_RANKINGS ? AP_RANKINGS[school] : 26;
         return {
           team: school,
           rank: index + 1,
-          apRank: r,
+          apRank: r !== 26 ? r : "-",
           diff: r - (index + 1),
         };
       });
