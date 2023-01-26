c6b9775c1b173484bf8f86121ff6495720159643
Lucas Goldman <58446351+lucasrgoldman@users.noreply.github.com>
2023-01-26 16:45:53 -0600

    Fix seeding (#25)
    * Fixed Seedings
    
    * Minor tweak

--- src/server/api/v1/lambda.cjs
+++ src/server/api/v1/lambda.cjs
@@ -64,31 +64,34 @@ module.exports = (app) => {
  */
 const generateBracket = (schools) => {
   const matchups = [];
-  for (let i = 0; i < 32; i++) {
-    const matchup = [];
-    let p = Math.floor(Math.random() * 100);
-    let r = (i % 8) + 1;
-    const higherSeed = {
-      rank: `No. ${r}`,
-      name: schools[i],
-      record: `${Math.floor(Math.random() * 35)}-${Math.floor(
-        Math.random() * 35
-      )}`,
-      percentile: p,
-      winner: false,
-    };
-    matchup.push(higherSeed);
-    const lowerSeed = {
-      rank: `No. ${17 - r}`,
-      name: schools[64 - i - 1],
-      record: `${Math.floor(Math.random() * 35)}-${Math.floor(
-        Math.random() * 35
-      )}`,
-      percentile: 100 - p,
-      winner: false,
-    };
-    matchup.push(lowerSeed);
-    matchups.push(matchup);
+  for (let i = 0; i < 4; i++) {
+    for (let j = 0; j < 8; j++) {
+      const matchup = [];
+      let p = Math.floor(Math.random() * 100);
+      let index = i + (j * 4);
+      let seed = j + 1;
+      const higherSeed = {
+        rank: `No. ${seed}`,
+        name: schools[index],
+        record: `${Math.floor(Math.random() * 35)}-${Math.floor(
+            Math.random() * 35
+        )}`,
+        percentile: p,
+        winner: false,
+      };
+      matchup.push(higherSeed);
+      const lowerSeed = {
+        rank: `No. ${17 - seed}`,
+        name: schools[63 - index],
+        record: `${Math.floor(Math.random() * 35)}-${Math.floor(
+            Math.random() * 35
+        )}`,
+        percentile: 100 - p,
+        winner: false,
+      };
+      matchup.push(lowerSeed);
+      matchups.push(matchup);
+    }
   }
   // round of 64, 32, 16, 8, 4, and 2
   return [matchups, [], [], [], [], []];
