e4cae48e98a658a0e2f83cfa23a3dc2893b321ff
Jack Harrington <47326372+jharring9@users.noreply.github.com>
2023-01-26 14:32:07 -0600

    Updated profile page #2

--- src/client/components/account-flow/Profile.js
+++ src/client/components/account-flow/Profile.js
@@ -47,7 +47,7 @@ export const Profile = ({ user }) => {
               name="email"
               id="email"
               className="mt-1 block w-full rounded-md border border-gray-300 py-2 px-3 shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
-              defaultValue={user.first}
+              defaultValue={user.email}
             />
           </div>
           <div className="col-span-12 sm:col-span-6">
