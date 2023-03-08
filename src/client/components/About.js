60d571a7b6c0e8ad91463f8cb7509a1f7909e073
Lucas Goldman <lucasgoldman@optonline.net>
2023-03-08 16:43:44 -0600

    Finalized about

--- src/client/components/About.js
+++ src/client/components/About.js
@@ -3,37 +3,28 @@ import { LoadingWrapper } from "./shared";
 
 const people = [
   {
-    name: "Jack Harrington",
-    role: "Founder",
-    imageUrl: "/assets/jack.jpg",
-    // bio: "Praesentium iure error aliquam voluptas ut libero. Commodi placeat sit iure nulla officiis. Ut ex sit repellat tempora. Qui est accusamus exercitationem natus ut voluptas. Officiis velit eos ducimus.",
-    twitterUrl: "#",
-    linkedinUrl: "#",
-  },
-  {
     name: "Lucas Goldman",
     role: "Founder",
     imageUrl: "/assets/lucas.jpg",
-    // bio: "Praesentium iure error aliquam voluptas ut libero. Commodi placeat sit iure nulla officiis. Ut ex sit repellat tempora. Qui est accusamus exercitationem natus ut voluptas. Officiis velit eos ducimus.",
-    twitterUrl: "#",
-    linkedinUrl: "#",
+    linkedinUrl: "https://www.linkedin.com/in/lucas-goldman/",
+  },
+  {
+    name: "Jack Harrington",
+    role: "Founder",
+    imageUrl: "/assets/jack.jpg",
+    linkedinUrl: "https://www.linkedin.com/in/jharrington22/",
   },
   {
     name: "Joel Dahan",
     role: "Founder",
     imageUrl: "/assets/joel.jpeg",
-    // bio: "Praesentium iure error aliquam voluptas ut libero. Commodi placeat sit iure nulla officiis. Ut ex sit repellat tempora. Qui est accusamus exercitationem natus ut voluptas. Officiis velit eos ducimus.",
-    twitterUrl: "#",
-    linkedinUrl: "#",
+    linkedinUrl: "https://www.linkedin.com/in/joel-dahan-9989851ba/",
   },
   {
     name: "Cason Stone",
     role: "Founder",
-    imageUrl:
-      "https://images.unsplash.com/photo-1505840717430-882ce147ef2d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=8&w=1024&h=1024&q=80",
-    // bio: "Praesentium iure error aliquam voluptas ut libero. Commodi placeat sit iure nulla officiis. Ut ex sit repellat tempora. Qui est accusamus exercitationem natus ut voluptas. Officiis velit eos ducimus.",
-    twitterUrl: "#",
-    linkedinUrl: "#",
+    imageUrl: "/assets/cason.jpg",
+    linkedinUrl: "https://www.linkedin.com/in/cason-stone-3761421b0/",
   },
   // More people...
 ];
@@ -251,29 +242,12 @@ export const About = () => {
                   <p className="text-base leading-7 text-gray-600">
                     {person.role}
                   </p>
-                  {/*<p className="mt-4 text-base leading-7 text-gray-600">*/}
-                  {/*  {person.bio}*/}
-                  {/*</p>*/}
                   <ul role="list" className="mt-6 flex gap-x-6">
                     <li>
                       <a
-                        href={person.twitterUrl}
-                        className="text-gray-400 hover:text-gray-500"
-                      >
-                        <span className="sr-only">Twitter</span>
-                        <svg
-                          className="h-5 w-5"
-                          aria-hidden="true"
-                          fill="currentColor"
-                          viewBox="0 0 20 20"
-                        >
-                          <path d="M6.29 18.251c7.547 0 11.675-6.253 11.675-11.675 0-.178 0-.355-.012-.53A8.348 8.348 0 0020 3.92a8.19 8.19 0 01-2.357.646 4.118 4.118 0 001.804-2.27 8.224 8.224 0 01-2.605.996 4.107 4.107 0 00-6.993 3.743 11.65 11.65 0 01-8.457-4.287 4.106 4.106 0 001.27 5.477A4.073 4.073 0 01.8 7.713v.052a4.105 4.105 0 003.292 4.022 4.095 4.095 0 01-1.853.07 4.108 4.108 0 003.834 2.85A8.233 8.233 0 010 16.407a11.616 11.616 0 006.29 1.84" />
-                        </svg>
-                      </a>
-                    </li>
-                    <li>
-                      <a
                         href={person.linkedinUrl}
+                        target="_blank"
+                        rel="noopener noreferrer"
                         className="text-gray-400 hover:text-gray-500"
                       >
                         <span className="sr-only">LinkedIn</span>
