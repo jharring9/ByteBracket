f3a9460a3ba2a34e6fd243fc0143050eb7f58fd7
Jack Harrington <47326372+jharring9@users.noreply.github.com>
2024-04-16 16:26:10 -0400

    Close leagues for offseason (#75)
    * Close leagues for start of games
    
    * Remove leagues for offseason

--- src/client/components/App.js
+++ src/client/components/App.js
@@ -14,12 +14,12 @@ import { Account } from "./Account";
 import { About } from "./About";
 import { useDispatch } from "react-redux";
 import { ViewBracket } from "./ViewBracket";
-import { ViewLeague } from "./leagues-flow/ViewLeague";
+// import { ViewLeague } from "./leagues-flow/ViewLeague";
 import { checkSession, fetchImages } from "./shared";
-import { Leagues } from "./Leagues";
-import { JoinLeague } from "./leagues-flow/JoinLeague";
+// import { Leagues } from "./Leagues";
+// import { JoinLeague } from "./leagues-flow/JoinLeague";
 import { PrivacyPolicy } from "./PrivacyPolicy";
-import { CreateLeague } from "./leagues-flow/CreateLeague";
+// import { CreateLeague } from "./leagues-flow/CreateLeague";
 
 /* Google Analytics */
 ReactGA.initialize("G-GR6LZND73X");
@@ -33,7 +33,7 @@ export default function App() {
   const [navigation, setNavigation] = useState([
     { name: "Home", href: "/home", current: false },
     { name: "Create Bracket", href: "/create", current: false },
-    { name: "Leagues", href: "/leagues", current: false },
+    // { name: "Leagues", href: "/leagues", current: false },
     { name: "About", href: "/about", current: false },
   ]);
 
@@ -55,10 +55,10 @@ export default function App() {
           <Route path="/register" element={<Register />} />
           <Route path="/account" element={<Account />} />
           <Route path="/about" element={<About />} />
-          <Route path="/leagues" element={<Leagues />} />
-          <Route path="/leagues/:id/:urlcode?" element={<ViewLeague />} />
-          <Route path="/join/:id/:code?" element={<JoinLeague />} />
-          <Route path="/newleague" element={<CreateLeague />} />
+          {/*<Route path="/leagues" element={<Leagues />} />*/}
+          {/*<Route path="/leagues/:id/:urlcode?" element={<ViewLeague />} />*/}
+          {/*<Route path="/join/:id/:code?" element={<JoinLeague />} />*/}
+          {/*<Route path="/newleague" element={<CreateLeague />} />*/}
           <Route path="/bracket/:user/:id" element={<ViewBracket />} />
           <Route path="/privacy" element={<PrivacyPolicy />} />
           <Route path="*" element={<NotFound />} />
